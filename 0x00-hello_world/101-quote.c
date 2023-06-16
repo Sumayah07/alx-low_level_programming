375530a85ec0694020d3b560fb15361f81e265c2
Sumayah07 <muhydeensumayah27@gmail.com>
2023-06-16 06:04:32 -0700

    create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - program entry point
+ * Return: 1 om success
+ */
+int main(void)
+{
+	write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
