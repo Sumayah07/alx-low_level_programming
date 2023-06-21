b864f261c566045979e2d3fc232aaef732209108
Sumayah07 <muhydeensumayah27@gmail.com>
2023-06-21 15:11:58 -0700

    create 1-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - This file must test that the function positive_or_negative()
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
